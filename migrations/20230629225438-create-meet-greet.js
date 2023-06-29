0cd3f9a043dcb95e579bbf2dd6f07c347b74d3e7
haydenn+1-at-933632846737 <haydenn@scigrp.com>
2023-06-29 19:12:07 -0400

    migration files updated

--- migrations/20230629225438-create-meet-greet.js
+++ migrations/20230629225438-create-meet-greet.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('MeetGreets', {
+    await queryInterface.createTable('meet_greets', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -10,31 +10,28 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       meet_greet_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       event_id: {
-        type: Sequelize.SMALLINT
+        type: Sequelize.SMALLINT,
+        allowNull: false
       },
       band_id: {
-        type: Sequelize.SMALLINT
+        type: Sequelize.SMALLINT,
+        allowNull: false
       },
       meet_start_time: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false
       },
       meet_end_time: {
-        type: Sequelize.DATE
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false
       }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('MeetGreets');
+    await queryInterface.dropTable('meet_greets');
   }
 };
--- migrations/20230629225439-create-stage.js
+++ migrations/20230629225439-create-stage.js
@@ -2,30 +2,20 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Stages', {
-      id: {
+    await queryInterface.createTable('stages', {
+      stage_id: {
+        type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      stage_id: {
-        type: Sequelize.INTEGER
+        primaryKey: true
       },
       stage_name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.STRING,
+        allowNull: false
       }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Stages');
+    await queryInterface.dropTable('stages');
   }
 };
